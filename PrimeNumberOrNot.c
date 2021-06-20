251449586e680914c0adc4af41223cb1ee3d73bd
Sivakumar Rajachidambaram <sivaram.ec@gmail.com>
2021-06-20 15:19:01 +0530

    Create PrimeNumberOrNot.c

---
+++ PrimeNumberOrNot.c
@@ -0,0 +1,19 @@
+int main()
+{
+     int n, i;
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+
+  for (i = 2; i <= n / 2; ++i) {
+    // condition for non-prime
+    if (n % i == 0) {
+     printf("%d is not a prime number.", n);
+      return 0;
+    }
+  }
+
+      printf("%d is a prime number.", n);
+
+
+  return 0;
+}
