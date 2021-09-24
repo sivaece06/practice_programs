de915daeabc5b1f23c208cc59e5f48273bc452ab
Sivakumar Rajachidambaram <sivaram.ec@gmail.com>
2021-09-24 05:50:53 +0530

    Update Bit_swap.c

--- Bit_swap.c
+++ Bit_swap.c
@@ -1,17 +1,25 @@
-//C code for swapping given bits of a number
 #include<stdio.h>
-int swapBits(int n, int p1, int p2)
+
+int swapBits(int n, int p1, int p2);
+
+int main () {
+    int n;
+    
+    scanf("%d", &n);
+    
+    printf("After swap: %d\n", swapBits(n,5,1) );
+}
+
+int swapBits(int n, int p, int q)
 {
     //left-shift 1 p1 and p2 times
     //and using XOR
-    n ^= 1 << p1;
-    n ^= 1 << p2;
+    
+    //swap only if bits are different
+    if (((n & (1 << p)) >> p) ^ ((n & (1 << q)) >> q)){
+        n ^= 1 << p;
+        n ^= 1 << q;
+    }
     return n;
 }
- 
-//Driver Code
-int main()
-{
-    printf("Result = %d", swapBits(5, 0, 1));
-    return 0;
-}
+
